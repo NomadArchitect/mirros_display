1bc7c4f13858592b20efcd3ba8ba8f0bfa7f9638
Tobias Grasse <tg@glancr.de>
2018-08-15 09:40:30 +0200

    Remove outdated fetching logic
    Was tailored to the prototype data structure
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@
         ></component>
         <component
           v-else
-          :is="moduleData.type"
+          :is="moduleData.id"
           :key="moduleData.id"
           v-bind:data="moduleData.attributes.data"
         ></component>
@@ -35,86 +35,37 @@
 </template>
 
 <script>
-  import Vue from 'vue'
-  import Hello from './components/Hello'
-  import Clock from './components/Clock'
-  import DataTable from './components/DataTable'
-  import List from './components/List'
-  import GenericHTML from './components/GenericHTML'
+import Clock from "./components/Clock";
+import DataTable from "./components/DataTable";
+import List from "./components/List";
+import GenericHTML from "./components/GenericHTML";
 
-  export default {
-    name: 'app',
-    components: {
-      Hello,
-      DataTable,
-      List,
-      GenericHTML
-    },
-    data: () => ({
-      data: [],
-      errors: [],
-      meta: {},
-      serverError: '',
-      creators: []
-    }),
-    created () {
-      // @TODO: Use final API URL and get rid of debug logging
-      // Get the data object from the backend API. Uses vue-axios.
-      this.$http.get('static/data.json')
-        .then(response => {
-          if (response.status !== 200) {
-            this.serverError = 'Cannot reach backend server: ' + response.statusText
-          } else {
-            // @TODO: Use ES6 destructuring magic to keep DRY
-            const responseData = response.data
-            // Scan module definitions if they contain a custom component that needs initialization.
-            responseData.data.forEach(module => {
-              const attr = module.attributes
-              if (module.type === 'dwtfyw') {
-                // Define the component.
-                // Register the component globally.
-              }
-              if (module.type === 'custom-inline') {
-                let components = {}
-                attr.uses.forEach(use => {
-                  if (use === 'Clock') {
-                    components = {Clock}
-                  }
-                })
-                // Get the custom template defined in the module object.
-
-                // Define the component.
-                const options = {
-                  template: attr.template,
-                  components: components,
-                  props: {
-                    title: String,
-                    subtitle: String,
-                    data: Object
-                  }
-                }
-                // Register the component globally.
-                Vue.component(attr.customType, Vue.extend(options))
-              }
-            })
-            console.log('last refresh:', responseData.meta.timestamp)
-            // Assign return object values to member variables. Yay, ES6!
-            Object.assign(this, responseData)
-          }
-        })
-        // After registering all custom components, add the data to the App to render it
-        .catch(error => {
-          this.serverError = 'Fatal error: ' + error
-        })
-    }
-  }
+export default {
+  name: "app",
+  components: {
+    DataTable,
+    List,
+    GenericHTML
+  },
+  data: () => ({
+    data: [],
+    errors: [],
+    serverError: ""
+  }),
+  created() {}
+};
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: white;
-  }
+body {
+  margin: 0;
+  background-color: black;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: white;
+}
 </style>
