3de8b5cbb594c3528f2acbe3c7061795761e1238
Tobias Grasse <tg@glancr.de>
2018-08-15 12:45:54 +0200

    Add mutations for default state slices to store.js
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- src/store.js
+++ src/store.js
@@ -4,7 +4,6 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  mutations: {},
   actions: {}
   state: {
     language: "enGb",
@@ -12,5 +11,28 @@ export default new Vuex.Store({
     widgetInstances: {},
     sourceInstances: {},
     recordLinks: {}
+  },
+  mutations: {
+    ADD_ERROR: (state, error) => {
+      state.errors = state.errors.push(error);
+    },
+    ADD_WIDGETS: (state, payload) => {
+      state.widgets = { ...state.widgets, ...payload };
+    },
+    ADD_WIDGETINSTANCES: (state, payload) => {
+      state.widgetInstances = { ...state.widgetInstances, ...payload };
+    },
+    ADD_SOURCEINSTANCES: (state, payload) => {
+      state.sourceInstances = { ...state.sourceInstances, ...payload };
+    },
+    ADD_RECORDLINKS: (state, payload) => {
+      state.recordLinks = { ...state.recordLinks, ...payload };
+    },
+    ADD_CALENDARS: (state, payload) => {
+      state.calendars = { ...state.calendars, ...payload };
+    },
+    ADD_REMINDERLISTS: (state, payload) => {
+      state.reminderLists = { ...state.reminderLists, ...payload };
+    }
   },
 });
