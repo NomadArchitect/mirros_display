5124140af1bdb3d57cf74f9e18dcb6a34781da5b
Tobias Grasse <tg@glancr.de>
2018-08-28 11:08:45 +0200

    Add/modify WIP templates for calendar widgets
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- public/templates/calendar_today.vue
+++ public/templates/calendar_today.vue
@@ -1,10 +1,13 @@
 <template>
     <table>
-        <tr v-for="event in events" :key="event.id">
-            <td v-if="event.allDay">ganztägig</td>
-            <td v-else>{{ formatDate(event.dtstart) }}</td>
-            <td>{{ event.summary }}</td>
-        </tr>
+      <tr v-if="!events.length">
+        <td>{{ t('Nothing on your schedule for today') }}</td>
+      </tr>
+      <tr v-for="event in events" :key="event.id">
+        <td v-if="event.allDay">ganztägig</td>
+        <td v-else>{{ formatDate(event.dtstart) }}</td>
+        <td>{{ event.summary }}</td>
+      </tr>
     </table>
 </template>
 
@@ -20,6 +23,11 @@ module.exports = {
       required: true
     }
   },
+  locales: {
+    deDe: {
+      "Nothing on your schedule for today": "Keine weiteren Termine für heute"
+    }
+  },
   data: function() {
     return {
       options: {
@@ -32,17 +40,7 @@ module.exports = {
     events: function() {
       return this.records.reduce((acc, record) => {
         const normalized = record.attributes.events
-          .filter(event => {
-            const eventDate = new Date(event.dtstart);
-            const now = new Date();
-            if (this.configuration.showPastEvents) {
-              return eventDate.getDate() === now.getDate();
-            } else {
-              return (
-                eventDate >= now && eventDate.getDate() < now.getDate() + 1
-              );
-            }
-          })
+          .filter(this.filterFn)
           .map(event => {
             event.calendar = record.attributes.name;
             return event;
@@ -58,6 +56,15 @@ module.exports = {
         hour12: this.configuration.timeformatChoice === "12"
       };
       return new Intl.DateTimeFormat("de-DE", options).format(Date.parse(date));
+    },
+    filterFn: function(event) {
+      const eventDate = new Date(event.dtstart);
+      const now = new Date();
+      if (this.configuration && this.configuration.showPastEvents) {
+        return eventDate.getDate() === now.getDate();
+      } else {
+        return eventDate >= now && eventDate.getDate() < now.getDate() + 1;
+      }
     }
   }
 };
