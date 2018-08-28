3ca6612fab28ade88617be73f53ef6676fad9419
Tobias Grasse <tg@glancr.de>
2018-08-28 11:07:31 +0200

    Filter record links by group id
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- src/components/WidgetInstanceWrapper.vue
+++ src/components/WidgetInstanceWrapper.vue
@@ -33,15 +33,22 @@ export default {
       // TODO: Can this be written more concise?
       const sourceInstances = this.widgetInstance.relationships.sourceInstances
         .data;
-      const links = sourceInstances
+      const records = sourceInstances
         .map(si => {
           return this.sourceInstances[si.id].relationships.recordLinks.data;
         })
-        .flat();
-      const records = links.map(link => {
-        const record = this.recordLinks[link.id].relationships.recordable.data;
-        return this.$store.state[record.type][record.id];
-      });
+        .flat()
+        .filter(link => {
+          return (
+            this.recordLinks[link.id].relationships.group.data.id ===
+            this.widgetInstance.relationships.group.data.id
+          );
+        })
+        .map(link => {
+          const record = this.recordLinks[link.id].relationships.recordable
+            .data;
+          return this.$store.state[record.type][record.id];
+        });
 
       return records;
     },
