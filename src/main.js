ece0d627de35cbe2bd3a8c8e5d00ff12033702be
Tobias Grasse <tg@glancr.de>
2018-11-02 12:23:29 +0100

    Add dependency vue-async-computed
    Makes asyncComputed available for widgets, which can then use the fetchAsset helper to compute icons and other assets from the backend.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ import appconfig from "@/appconfig";
 import axios from "axios";
 
 import VueTranslate from "vue-translate-plugin";
+import AsyncComputed from "vue-async-computed";
 
 import "@/assets/sass/global.scss";
 
 Vue.config.productionTip = false;
 Vue.use(VueTranslate);
+Vue.use(AsyncComputed);
 
 axios.defaults.baseURL = appconfig.backendUrl;
 axios.defaults.headers.common["Content-Type"] = "application/vnd.api+json";
