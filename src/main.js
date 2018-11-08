0ffa40035fcc8819c952142b6da15a7d0fb6f267
Tobias Grasse <tg@glancr.de>
2018-11-08 14:37:49 +0100

    Provide vue-axios to widgets
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import store from "@/store";
 import appconfig from "@/appconfig";
 
 import axios from "axios";
+import VueAxios from "vue-axios";
 
 import VueTranslate from "vue-translate-plugin";
 import AsyncComputed from "vue-async-computed";
@@ -13,6 +14,7 @@ import "@/assets/sass/global.scss";
 Vue.config.productionTip = false;
 Vue.use(VueTranslate);
 Vue.use(AsyncComputed);
+Vue.use(VueAxios, axios);
 
 axios.defaults.baseURL = appconfig.backendUrl;
 axios.defaults.headers.common["Content-Type"] = "application/vnd.api+json";
