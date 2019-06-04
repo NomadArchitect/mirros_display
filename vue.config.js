841b129d3f1503051164d7abaac162b38760a46f
Tobias Grasse <tg@glancr.de>
2019-06-04 18:41:03 +0200

    Adapt vue.config.js to newer property names
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   runtimeCompiler: true,
-  baseUrl: process.env.NODE_ENV === "production" ? "/display/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/display/" : "/",
   chainWebpack: config => {
     const svgRule = config.module.rule("svg");
 
