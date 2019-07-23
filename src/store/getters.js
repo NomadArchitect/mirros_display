5d7b6c41bb64841b5b7482b493f4391c151c73f5
Tobias Grasse <tg@glancr.de>
2019-07-23 09:59:30 +0200

    Refactor recordsForWidgetInstance getter
    Since records are attached to the sourceInstance model now, the filter logic can be simplified a lot.
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- src/store/getters.js
+++ src/store/getters.js
@@ -7,57 +7,25 @@ export default {
   widgetForInstance: state => instance => {
     return state.widgets[instance.relationships.widget.data.id];
   },
-  // FIXME: This is some monstrous filter logic.
-  recordsForWidgetInstance: (state, getters) => instance => {
-    // Return early if the widget has no group, and thus no records.
-    if (instance.relationships.group === null) return [];
+  /**
+   * Returns all sub-resources that have been chosen from the associated source instance's records.
+   */
+  recordsForWidgetInstance: state => instance => {
+    const associations = instance.relationships.instanceAssociations.data
+      .map(ia => state.instanceAssociations[ia.id])
+      .filter(assoc => assoc != undefined);
 
-    // TODO: Can this be written more concise?
-    const sourceInstances = instance.relationships.sourceInstances.data;
-
-    let records = [];
-    for (let si of sourceInstances) {
-      if (state.sourceInstances[si.id]) {
-        records.push(
-          state.sourceInstances[si.id].relationships.recordLinks.data
-        );
-      }
-    }
-    // .flat() not supported by WPE fork yet
-    records = [].concat
-      .apply([], records)
-      .filter(link => {
-        // TODO: Maybe restructure the data model so that groups encapsulate their records, making this filtering unnecessary
-        return (
-          state.recordLinks[link.id].relationships.group.data.id ===
-          instance.relationships.group.data.id
-        );
-      })
-      .map(link => {
-        const record = state.recordLinks[link.id].relationships.recordable.data;
-        return state[record.type][record.id];
+    const records = associations.reduce((acc, assoc) => {
+      const sid = assoc.relationships.sourceInstance.data.id;
+      const gid = assoc.relationships.group.data.id;
+      assoc.attributes.configuration.chosen.forEach(choice => {
+        if (state.sourceInstances[sid].attributes.records[gid] != undefined) {
+          acc.push(state.sourceInstances[sid].attributes.records[gid][choice]);
+        }
       });
+      return acc;
+    }, []);
 
-    if (getters.widgetForInstance(instance).attributes.singleSource) {
-      return records;
-    } else {
-      const chosenSubresources = [].concat(
-        ...instance.relationships.instanceAssociations.data.reduce(
-          (acc, ia) => {
-            if (state.instanceAssociations[ia.id]) {
-              acc.push(
-                state.instanceAssociations[ia.id].attributes.configuration
-                  .chosen
-              );
-            }
-            return acc;
-          },
-          []
-        )
-      );
-      return records.filter(record => {
-        return chosenSubresources.includes(record.attributes.uid);
-      });
-    }
+    return records;
   }
 };
