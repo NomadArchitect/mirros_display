f6685b34fc43f4b548fb209b7619e853318907ee
Tobias Grasse <tg@glancr.de>
2021-12-22 15:55:52 +0100

    fix: Drop babel-eslint from eslintrc
    Also forces newest es2021 environment
    
    Signed-off-by: Tobias Grasse <tg@glancr.de>

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,12 @@ module.exports = {
 
   env: {
     node: true,
+    es2021: true,
   },
 
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn",
-  },
-
-  parserOptions: {
-    parser: "babel-eslint",
   },
 
   extends: [
